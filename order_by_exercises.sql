a3c53fb2afa21528e7dc0c8cd3305b3edc1e27b8
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-09-08 15:03:30 -0500

    review/corrections for order_by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -13,7 +13,7 @@ USE employees;
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya','Maya')
-	ORDER BY first_name;
+ORDER BY first_name;
 
 /* 3 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned 
 by first name and then last name. In your comments, answer: What was the first and last name in the 
@@ -24,7 +24,7 @@ first row of the results? What was the first and last name of the last person in
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya','Maya')
-	ORDER BY first_name,last_name;
+ORDER BY first_name,last_name;
 
 /* 4 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned 
 by last name and then first name. In your comments, answer: What was the first and last name in the 
@@ -35,30 +35,31 @@ first row of the results? What was the first and last name of the last person in
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya','Maya')
-	ORDER BY last_name,first_name;
+ORDER BY last_name,first_name;
     
 /* 5 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results 
 by their employee number. Enter a comment with the number of employees returned, the first employee 
 number and their first and last name, and the last employee number with their first and last name.
 	899 employees were returned.
-    The first employee is Ramza Erde.
-    The last empoyee is Tadahiro Erde		*/
+    The first employee is Ramza Erde (employee number 10021) .
+    The last empoyee is Tadahiro Erde (employee	number 499648)	*/
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%E' 
-	ORDER BY emp_no;
+ORDER BY emp_no;
     
 /* 6 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results 
 by their hire date, so that the newest employees are listed first. Enter a comment with the number of 
 employees returned, the name of the newest employee, and the name of the oldest employee.
-	The newest employee is Teiji Eldridge hired 1999-11-27.
+	(899 employees returned)
+    The newest employee is Teiji Eldridge hired 1999-11-27.
     The oldest employee is Sergi Erde hired 1985-02-02.		*/
     
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%E' 
-	ORDER BY hire_date DESC;
+ORDER BY hire_date DESC;
 
 /* 7 Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest 
 employee who was hired last is the first result. Enter a comment with the number of employees returned,
@@ -71,4 +72,4 @@ employee who was hired last is the first result. Enter a comment with the number
 SELECT *
 FROM employees
 WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
-	ORDER BY birth_date,hire_date DESC;
+ORDER BY birth_date,hire_date DESC;
