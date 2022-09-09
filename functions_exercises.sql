3e03b9d77334b0bdddd6107ef76b2af8cc3f4fef
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-09-09 09:58:00 -0500

    updated file after review, corrected salary question and syntax

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -30,6 +30,14 @@ WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%';
 SELECT MAX(salary), MIN(salary)
 FROM salaries;
 
+#CORRECTION for current salary
+SELECT MAX(salary), 
+	MIN(salary)
+FROM salaries
+WHERE to_date >CURDATE();
+
+
+
 /* 6 Use your knowledge of built in SQL functions to generate a username for all of the employees. 
 A username should be all lowercase, and consist of the first character of the employees first name, 
 the first 4 characters of the employees last name, an underscore, the month the employee was born, and the 
@@ -53,4 +61,19 @@ last two digits of the year that they were born. Below is an example of what the
 	
 SELECT CONCAT(SUBSTR(LOWER(first_name), 1,1),SUBSTR(LOWER(last_name),1,4),'_', SUBSTR(birth_date,6,2),SUBSTR(birth_date,3,2)),
 first_name, last_name, birth_date
+FROM employees;
+
+# more elegant syntax
+
+SELECT first_name,
+	last_name,
+    LOWER(
+		CONCAT(
+			SUBSTR(first_name, 1,1),
+            SUBSTR(last_name,1,4),
+            '_',
+            SUBSTR(birth_date, 6,2),
+            SUBSTR(birth_date, 3,2)
+            )
+		)AS username
 FROM employees;
